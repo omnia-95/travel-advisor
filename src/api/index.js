2a9e64b5e158a3d53a7ea1a5fb973a65ab8d83ae
Omnia Abdulnabi <omnia.baghian@yahoo.com>
2024-03-11 03:10:28 +0300

    fetch attractions from RapidAPI

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,77 @@
+// import axios from 'axios';
+
+// const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+
+// const options = {
+//     params: {
+//         bl_latitude: '11.847676',
+//         tr_latitude: '12.838442',
+//         bl_longitude: '109.095887',
+//         tr_longitude: '109.149359',
+//     },
+//     headers: {
+//         'X-RapidAPI-Key': 'ebcf5818cdmsh14c4e6cd6b14d28p141a03jsna85055a883ba',
+//         'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+//     }
+// };
+
+
+// export const getPlacesData = async () => {
+//     try {
+//         const { data: { data } } = await axios.get(URL, options);
+
+//         return data;
+
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+
+
+
+// import axios from 'axios';
+
+// const URL = 'https://travel-advisor.p.rapidapi.com/attractions/list'
+
+// const options = {
+//     params: {
+//         location_id: '298571',
+//         sort: 'recommended'
+//     },
+//     headers: {
+//         'X-RapidAPI-Key': 'ebcf5818cdmsh14c4e6cd6b14d28p141a03jsna85055a883ba',
+//         'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+//     }
+// };
+
+// export const getPlacesData = async () => {
+//     try {
+//         const { data: { data } } = await axios.get(URL, options);
+
+//         return data;
+
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+
+
+
 import axios from 'axios';
 
-const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+const URL = 'https://travel-advisor.p.rapidapi.com/locations/search'
 
 const options = {
     params: {
-        bl_latitude: '11.847676',
-        tr_latitude: '12.838442',
-        bl_longitude: '109.095887',
-        tr_longitude: '109.149359',
+        query: 'dubai',
     },
     headers: {
         'X-RapidAPI-Key': 'ebcf5818cdmsh14c4e6cd6b14d28p141a03jsna85055a883ba',
         'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
     }
 };
-
 
 export const getPlacesData = async () => {
     try {
@@ -26,3 +83,7 @@ export const getPlacesData = async () => {
         console.log(error)
     }
 }
+
+
+// category: {key: "attraction", name: "Attraction"}
+
