b047e223828a4f5738db7516185fd3f3dc98dcc1
Omnia Abdulnabi <omnia.baghian@yahoo.com>
2024-03-12 14:11:23 +0300

    work on Placedetails in list cards

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -18,10 +18,31 @@ const PlaceDetails = ({ place }) => {
                 title={place.name}
             />
             <CardContent>
-                <Typography gutterBottom variant='h5'> {place.name} </Typography>
+                <Typography gutterBottom variant="h5"> {place.name} </Typography>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1"> Price </Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+                </Box>
+                {place?.address && (
+                    <Typography gutterBottom variant="subtitle2" color="textSecondary" className={classes.subtitle}>
+                        <LocationOnIcon /> {place.address}
+                    </Typography>
+                )}
+                {place?.phone && (
+                    <Typography gutterBottom variant="subtitle2" color="textSecondary" className={classes.subtitle}>
+                        <PhoneIcon /> {place.phone}
+                    </Typography>
+                )}
+                <CardActions>
+                    <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')}>
+                        Trip Advisor
+                    </Button>
+                    <Button size="small" color="primary" onClick={() => window.open(place.website, '_blank')}>
+                        Website
+                    </Button>
+                </CardActions>
             </CardContent>
-
-        </Card>
+        </Card >
     );
 }
 
